c9aa696ec317fd5d1e01ad21ea04ea4d3f20ae8e
smart-life-tech <christlightworld@gmail.com>
2023-02-08 22:46:43 +0100

    modified:   pages/_app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
-import React, {useMemo} from "react";
-import {WalletAdapterNetwork} from "@solana/wallet-adapter-base";
-import {WalletModalProvider} from "@solana/wallet-adapter-react-ui";
-import {ConnectionProvider, WalletProvider} from "@solana/wallet-adapter-react";
+import React, { useMemo } from "react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
 import {
   GlowWalletAdapter,
   PhantomWalletAdapter
 } from "@solana/wallet-adapter-wallets";
-import {clusterApiUrl} from "@solana/web3.js";
+import { clusterApiUrl } from "@solana/web3.js";
 
 // require('../styles/globals.css');
 require('../styles/Home.module.css');
@@ -15,10 +15,10 @@ require('@solana/wallet-adapter-react-ui/styles.css');
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  const network = WalletAdapterNetwork.Mainnet ; 
+  const network = WalletAdapterNetwork.Mainnet;
 
- // const endpoint = useMemo(()=> clusterApiUrl(network), [network]);
- const endpoint = useMemo(()=>"https://cors-anywhere.herokuapp.com/https://solana-api.projectserum.com",[network]);
+  // const endpoint = useMemo(()=> clusterApiUrl(network), [network]);
+  const endpoint = useMemo(() => "https://cors-anywhere.herokuapp.com/https://solana-api.projectserum.com", [network]);
   const wallets = useMemo(
     () => [
       new PhantomWalletAdapter(),
@@ -26,14 +26,14 @@ function MyApp({ Component, pageProps }) {
     ],
     [network]
   )
-  return( 
-  <ConnectionProvider endpoint={endpoint}>
-    <WalletProvider wallets={wallets} autoConnect>
-      <WalletModalProvider>
-        <Component {...pageProps} />
-      </WalletModalProvider>        
-    </WalletProvider>    
-  </ConnectionProvider>
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Component {...pageProps} />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   )
 }
 
